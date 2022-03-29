ddc0274f69cca548acfff83a9583fa0669868be6
Daniel D. Hromada <hromi@lesen.digital>
2022-03-29 16:40:45 +0200

    HTML/JS Websocket client

--- client/client.py
+++ client/client.py
@@ -1,7 +1,7 @@
 import websocket,sys
     
 ws = websocket.WebSocket()
-ws.connect("ws://"+sys.argv[1]+":12345/api/v1/stt")
+ws.connect("wss://"+sys.argv[1]+":12345/api/v1/stt")
 
 with open(sys.argv[2], mode='rb') as file:  # b is important -> binary
     audio = file.read()
--- coqui-server/app.py
+++ coqui-server/app.py
@@ -32,17 +32,15 @@ async def stt(request, ws):
     logger.debug(f"Received {request.method} request at {request.path}")
     try:
         audio = await ws.recv()
-
         inference_start = perf_counter()
         text = await app.loop.run_in_executor(executor, lambda: engine.run(audio))
         inference_end = perf_counter() - inference_start
-
         await ws.send(json.dumps(Response(text, inference_end).__dict__))
         logger.debug(f"Completed {request.method} request at {request.path} in {inference_end} seconds")
+        logger.debug(text)
     except Exception as e:  # pylint: disable=broad-except
         logger.debug(f"Failed to process {request.method} request at {request.path}. The exception is: {str(e)}.")
         await ws.send(json.dumps(Error("Something went wrong").__dict__))
-
     await ws.close()
 
 
@@ -50,6 +48,10 @@ if __name__ == "__main__":
     app.run(
         host=conf["server.http.host"],
         port=conf["server.http.port"],
+        ssl=dict(
+            cert=conf["server.http.cert_path"],
+            key=conf["server.http.key_path"]
+        ),
         access_log=True,
         debug=True,
     )
